e77abb5ed4dca536dc0dbd4db481406605d5e596
7erra <38866692+7erra@users.noreply.github.com>
2022-01-14 19:11:10 +0100

    functions addon added

---
+++ addons/functions/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ADDON
+	{
+		name = CSTRING(component);
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {};
+		author = "BIKI Contributors";
+		authors[] = {};
+		url = "https://community.bistudio.com/wiki/Main_Page";
+		VERSION_CONFIG;
+	};
+};
+
+#include "CfgFunctions.hpp"
---
+++ addons/functions/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT functions
+#include "script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_MAIN
+	#define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_FUNCTIONS
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
+#endif
+
+#include "script_macros.hpp"
