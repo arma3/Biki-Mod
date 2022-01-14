05b34f1a70187160f423c458db41372c05c74d91
7erra <38866692+7erra@users.noreply.github.com>
2022-01-14 01:29:22 +0100

    init

---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,7 @@
+#define PREFIX biki
+#include "script_version.hpp"
+#define VERSION MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
+
+#define REQUIRED_VERSION 2.06
