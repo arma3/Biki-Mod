0151202c8b159b1a5b6d8cd5f621f8d53c3b7329
7erra <38866692+7erra@users.noreply.github.com>
2022-01-15 21:54:33 +0100

    created mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name 			= "BIKI Mod";
+author 			= "BIKI Contributors";
+logo 			= "";
+logoOver 		= "";
+tooltip 		= "Bohemia Interactive Community Wiki Mod";
+tooltipOwned 	= "";
+picture 		= "";
+actionName 		= "BIKI";
+action 			= "https://community.bistudio.com/wiki/Main_Page";
+overview 		= "A mod that is used to maintain the BIKI.";
