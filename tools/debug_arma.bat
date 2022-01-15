41c0674ffbed71aef9e4c612679b1b68de3ad724
7erra <38866692+7erra@users.noreply.github.com>
2022-01-15 18:12:50 +0100

    Build project before launching, more mods

--- tools/debug_arma.bat
+++ tools/debug_arma.bat
@@ -1,7 +1,13 @@
-set "arma_path=D:\SteamLibraryM\steamapps\common\Arma 3"
-set arma_exe=arma3diag_x64.exe
-set "mods="!Workshop\@CBA_A3;P:\BIKI-Mod""
+@echo off
+:: Project related setup
+cd P:\BIKI-Mod
+python tools\generate_cfgfunctions.py functions
+hemtt build
 
-cd /d %arma_path%
+:: Start Arma 3 (diag)
+set "arma_path=D:\SteamLibrary\steamapps\common\Arma 3"
+set arma_exe=arma3diag_x64.exe
+set "mods="!Workshop\@CBA_A3;!Workshop\@Advanced Developer Tools;!Workshop\@Pythia;!Workshop\@7erra's Editing Extensions;!Workshop\@3den Enhanced;P:\BIKI-Mod""
+cd /d "%arma_path%"
 start %arma_exe% -debug -filePatching -noSplash -mod=%mods% -skipIntro -world=empty
 exit
